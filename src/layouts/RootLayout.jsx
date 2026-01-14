b3d699790038d1ff35c954b23265fc801a51d9aa
Asif-vai <mr3366533@gmail.com>
2026-01-14 11:31:33 +0600

    create navbar

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../shared/Navbar/Navbar";
 
 function RootLayout() {
   return (
     <div>
-      <header>header</header>
-      <main>main</main>
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <main>
+        <Outlet/>
+      </main>
       <footer>footer</footer>
     </div>
   );
---
+++ src/shared/Navbar/Navbar.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { Link, NavLink } from "react-router";
+
+function Navbar() {
+  const links = (
+    <>
+      <li>
+        <NavLink>Home</NavLink>
+      </li>
+      <li>
+        <NavLink>Blogs</NavLink>
+      </li>
+      <li>
+        <NavLink>About</NavLink>
+      </li>
+      <li>
+        <NavLink>Contact</NavLink>
+      </li>
+    </>
+  );
+  return (
+    
+      <div className="navbar shadow-sm bg-base-100 md:px-6 space-x-3">
+        <div className="navbar-start ">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+          <Link to='/' className="">
+            <img src="./logo.png" alt="" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end gap-4">
+          <label className="input ">
+            <svg
+              className="h-[1em] opacity-50 "
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" required placeholder="Search"/>
+          </label>
+         
+        </div>
+      </div>
+  );
+}
+
+export default Navbar;
