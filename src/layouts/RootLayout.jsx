73b23ead6eb3e2f03c64258249eb952f117187c8
Asif-vai <mr3366533@gmail.com>
2026-01-14 11:03:28 +0600

    configure routes

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function RootLayout() {
+  return (
+    <div>
+      <header>header</header>
+      <main>main</main>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default RootLayout;
